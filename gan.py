44e8a28ecd64791309d21c96b19f0c7613fdc52d
bachlm <maximilian.bachl@kapsch.net>
2023-07-31 10:34:04 +0200

    Add evaluation

--- gan.py
+++ gan.py
@@ -4,6 +4,12 @@ from tensorflow.keras import layers
 from datetime import datetime
 import os
 from common import *
+import argparse
+import cv2
+import numpy as np
+import random
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 dataset_name = 'dataset'
 # dataset_name = 'dataset-capitals'
@@ -17,6 +23,13 @@ multiplier = 6
 
 epochs = 15
 lr = 1e-4
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-m', '--mode', default='train')      # option that takes a value
+
+args = parser.parse_args()
+print(args)
 
 def make_generator_model():
   gen = tf.keras.Sequential(
@@ -76,23 +89,6 @@ def augment_simple(image, label):
     # tf.Assert(tf.reduce_any(image < 0.), [image])
     return image, label
 
-dataset = tf.keras.utils.image_dataset_from_directory(
-    dataset_name,
-    seed=0,
-    shuffle=True,
-    batch_size=None,
-    color_mode='grayscale',
-    image_size=image_shape                                            
-).map(augment_simple).batch(batch_size, drop_remainder=True)
-
-logdir = "gan_logs/" + datetime.now().strftime("%Y%m%d-%H%M%S") + '/'
-os.makedirs(logdir, exist_ok=True)
-file_name = __file__.split('/')[-1]
-with open(__file__) as fr:
-   content = fr.read()
-with open(logdir + file_name, 'w') as fw:
-   fw.write(content)
-file_writer = tf.summary.create_file_writer(logdir)
 seed = tf.concat([tf.zeros([num_chars, noise_dim]), tf.one_hot(tf.range(0, num_chars), depth=num_chars)], axis=-1)
 
 class CustomCallback(tf.keras.callbacks.Callback):
@@ -112,7 +108,7 @@ class CustomCallback(tf.keras.callbacks.Callback):
 
       with file_writer.as_default():
         tf.summary.image("In imgs", (self.model.val_data+1)/2, step=epoch, max_outputs=64)
-            
+
     with file_writer.as_default():
       tf.summary.scalar('lr', self.model.lr_decayed_fn(epoch*batches_per_epoch), step=epoch)
 
@@ -143,15 +139,15 @@ class CustomModel(tf.keras.Model):
     self.sparse_categorical_cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
 
     self.generator = make_generator_model()
-    print(self.generator.summary())
+    # print(self.generator.summary())
     self.discriminator = make_discriminator_model()
-    print(self.discriminator.summary())
+    # print(self.discriminator.summary())
     self.classifier_model = model
     # self.classifier_model.load_weights('logs/20230704-173202/weights.47')
     # self.classifier_model.load_weights('logs/20230721-120604/weights.29')
     # self.classifier_model.load_weights('logs/20230723-175517/weights.23')
     self.classifier_model.load_weights('logs/20230723-175517/weights.26')
-    print(self.classifier_model.summary())
+    # print(self.classifier_model.summary())
 
   def generate(self):
     results_tuple = []
@@ -199,7 +195,7 @@ class CustomModel(tf.keras.Model):
           tf.debugging.Assert(tf.reduce_all(augmented_imgs >= -1), [augmented_imgs])
           class_loss = \
             self.sparse_categorical_cross_entropy(
-              labels, 
+              labels,
               self.classifier_model(augmented_imgs, training=False))
           # class_loss = 0
           disc_loss = self.discriminator_loss(real_output, fake_output)
@@ -219,26 +215,117 @@ class CustomModel(tf.keras.Model):
       self.class_loss_tracker.update_state(tf.reduce_mean(class_loss))
 
       return {
-        "gen_loss": self.gen_loss_tracker.result(), 
-        "disc_loss": self.disc_loss_tracker.result(), 
+        "gen_loss": self.gen_loss_tracker.result(),
+        "disc_loss": self.disc_loss_tracker.result(),
         "class_loss": self.class_loss_tracker.result()
       }
 
 model = CustomModel()
 model.compile(run_eagerly=True)
 
-# steps_per_epoch = 1000
-batches_per_epoch = len(dataset)
+def analyze(raw_imgs, out_path, font_name):
+  accuracies = []
+  big_square = np.zeros([64*8, 64*8], dtype=np.uint8)
+  for j, img in enumerate(raw_imgs):
+    big_square[(j//8)*64:((j//8)+1)*64, (j%8)*64:((j%8)+1)*64] = np.squeeze(img.numpy().astype(np.uint8))
+  for _ in range(100):
+    imgs, _ = list(zip(*[augment(img, tf.zeros([])) for img in raw_imgs]))
+    imgs = tf.stack(imgs, axis=0)
+    predictions = tf.math.argmax(model.classifier_model(imgs, training=False), axis=-1, output_type=tf.int32)
+    tf.Assert(predictions.shape[0] == imgs.shape[0], [f'{len(predictions.shape)}, {len(imgs)}'])
+    accuracy = tf.reduce_mean(tf.cast(tf.range(len(imgs)) == predictions, dtype=tf.float32))
+    accuracies.append(accuracy.numpy().item())
+  accuracy = np.mean(accuracies)
+  cv2.imwrite(f'{out_path}/{font_name}.png', big_square)
+  return accuracy
 
-model.fit(
-    dataset, 
-    steps_per_epoch=batches_per_epoch,
-    epochs=int(epochs*(len(dataset)/batches_per_epoch)),
-    shuffle=False,
-    callbacks=[
-        tf.keras.callbacks.ModelCheckpoint(
-            os.path.join(logdir, "weights.{epoch:02d}"), verbose=1, save_weights_only=True),
-        tf.keras.callbacks.TensorBoard(log_dir=logdir),
-        CustomCallback()
-    ]
-)
+if args.mode == 'train':
+    dataset = tf.keras.utils.image_dataset_from_directory(
+        dataset_name,
+        seed=0,
+        shuffle=True,
+        batch_size=None,
+        color_mode='grayscale',
+        image_size=image_shape
+    ).map(augment_simple).batch(batch_size, drop_remainder=True)
+
+    logdir = "gan_logs/" + datetime.now().strftime("%Y%m%d-%H%M%S") + '/'
+    os.makedirs(logdir, exist_ok=True)
+    file_name = __file__.split('/')[-1]
+    with open(__file__) as fr:
+      content = fr.read()
+    with open(logdir + file_name, 'w') as fw:
+      fw.write(content)
+    file_writer = tf.summary.create_file_writer(logdir)
+
+    # steps_per_epoch = 1000
+    batches_per_epoch = len(dataset)
+
+    model.fit(
+        dataset,
+        steps_per_epoch=batches_per_epoch,
+        epochs=int(epochs*(len(dataset)/batches_per_epoch)),
+        shuffle=False,
+        callbacks=[
+            tf.keras.callbacks.ModelCheckpoint(
+                os.path.join(logdir, "weights.{epoch:02d}"), verbose=1, save_weights_only=True),
+            tf.keras.callbacks.TensorBoard(log_dir=logdir),
+            CustomCallback()
+        ]
+    )
+elif args.mode == 'export':
+    # Current max accuracy is 0.986
+    path = 'gan_logs/20230728-142502/'
+    all_weights = [item[:-len('.index')] for item in os.listdir(path) if '.index' in item]
+    out_dir = path.split('/')[-1]
+    out_path = 'out_imgs/' + out_dir
+    os.makedirs(out_path, exist_ok=True)
+    for i, weight_file_name in enumerate(all_weights): 
+      model.load_weights(path + weight_file_name)
+      out_imgs = model.generator(tf.concat([seed, tf.tile(tf.reshape(1.0, [1, 1]), (seed.shape[0], 1))], axis=-1), training=False)
+      out_imgs = tf.squeeze(out_imgs)
+      os.makedirs(f'{out_path}/{weight_file_name}', exist_ok=True)
+      raw_imgs = []
+      for j, item in enumerate(tf.unstack(out_imgs, axis=0)):
+        converted = np.round((item.numpy()+1.)/2.*255).astype(np.uint8)
+        cv2.imwrite(f'{out_path}/{weight_file_name}/{j}.png', converted)
+        raw_imgs.append(tf.reshape(tf.constant(converted, dtype=tf.float32), (64, 64, 1)))
+      analyze(raw_imgs, out_path, weight_file_name)
+elif args.mode == 'classify_fonts':
+  out_file_name = 'font_accuracies.txt'
+  all_accuracies = []
+  current_max = 0
+  if os.path.isfile(out_file_name):
+    with open(out_file_name) as fr:
+      all_accuracies = [float(item) for item in [item.strip() for item in fr.read().split('\n')] if len(item) > 0]
+  with open(out_file_name, 'a', buffering=1) as fw:
+    fonts_to_analyze = num_fonts
+    all_fonts = list(range(num_fonts))
+    random.seed(0)
+    random.shuffle(all_fonts)
+    for font_index in tqdm(range(fonts_to_analyze)):
+      if font_index < len(all_accuracies):
+        print(f'Skipping {font_index}')
+        continue
+      raw_imgs = []
+      for char_index in range(num_chars):
+        read_img = cv2.imread(f'dataset/{str(char_index).zfill(2)}/{all_fonts[font_index]}.png')
+        assert np.all(read_img[:,:,0] == read_img[:,:,1]) and np.all(read_img[:,:,0] == read_img[:,:,2])
+        # print(f'{read_img.shape=}')
+        raw_img = tf.reshape(tf.constant(read_img[:,:,0], dtype=tf.float32), (64, 64, 1))
+        raw_imgs.append(raw_img)
+      font_name = str(font_index).zfill(2)
+      accuracy = analyze(raw_imgs, f'out_imgs/', font_name)
+      current_max = max(current_max, accuracy)
+      print(f'{font_name=}, {accuracy=}, {current_max=}')
+      all_accuracies.append(accuracy)
+      fw.write(f'{accuracy}\n')
+  plt.figure()
+  counts, bins = np.histogram(all_accuracies, 40)
+  counts = counts/np.sum(counts)
+  print(f'{np.max(all_accuracies)=}, {np.median(all_accuracies)=}, {np.mean(all_accuracies)=}')
+  print('counts', counts, 'bins', bins)
+  plt.stairs(counts, bins, fill=True)
+  plt.yticks([], [])
+  plt.tight_layout()
+  plt.savefig('histogram.pdf')
